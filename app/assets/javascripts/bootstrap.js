942f839b98f87abf28cdeedf6add5fbf41a99506
Alex Neigher <alex.neigher@snapdocs.com>
2017-03-21 16:56:31 -0700

    spinner uI on load, and safer rendering of missing images

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -3,10 +3,3 @@ jQuery(function() {
   $("a[rel~=popover], .has-popover").popover();
   return $("a[rel~=tooltip], .has-tooltip").tooltip();
 });
-
-//globally, show spinner on form submit
-$('form').on('submit', function(){
-  original_text = $(this).find('.btn').html;
-  alert(original_text)
-  //bind_ajax_success($this, original_text);
-})
